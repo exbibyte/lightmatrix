590aaa7068dbb980fb2e917558bed748554def2b
Yuan Liu <yuan6liu@gmail.com>
2021-09-16 01:20:30 -0700

    added more things

---
+++ src/README.md
@@ -0,0 +1,2 @@
+# light weight matrix
+- utility functions for small 2D matrices, currently using array as backing
--- src/matrix.rs
+++ src/matrix.rs
@@ -58,8 +58,8 @@ macro_rules! impl_partialeq_integral {
             for Matrix<$type, ROW, COL>
         {
             fn eq(&self, other: &Self) -> bool {
-                for i in 0..self.0.len() {
-                    for j in 0..self.0[0].len() {
+                for i in 0..ROW {
+                    for j in 0..COL {
                         if self.0[i][j] != other.0[i][j] {
                             return false;
                         }
@@ -77,8 +77,8 @@ macro_rules! impl_partialeq_float {
             for Matrix<$type, ROW, COL>
         {
             fn eq(&self, other: &Self) -> bool {
-                for i in 0..self.0.len() {
-                    for j in 0..self.0[0].len() {
+                for i in 0..ROW {
+                    for j in 0..COL {
                         if !self.0[i][j].is_finite() || !other.0[i][j].is_finite() {
                             return false;
                         } else if (self.0[i][j] - other.0[i][j]).abs() > Float::epsilon() {
@@ -95,9 +95,35 @@ macro_rules! impl_partialeq_float {
 impl_partialeq_integral!(i8);
 impl_partialeq_integral!(i32);
 impl_partialeq_integral!(i64);
+impl_partialeq_integral!(i128);
+impl_partialeq_integral!(u8);
+impl_partialeq_integral!(u32);
+impl_partialeq_integral!(u64);
+impl_partialeq_integral!(u128);
+
 impl_partialeq_float!(f32);
 impl_partialeq_float!(f64);
 
 impl<const ROW: usize, const COL: usize> Eq for Matrix<i8, ROW, COL> {}
 impl<const ROW: usize, const COL: usize> Eq for Matrix<i32, ROW, COL> {}
 impl<const ROW: usize, const COL: usize> Eq for Matrix<i64, ROW, COL> {}
+
+impl<T: NumAssign + Copy + Default, const ROW: usize, const COL: usize> Matrix<T, ROW, COL> {
+    fn t(&self) -> Matrix<T, COL, ROW> {
+        let mut res = Matrix([[T::default(); ROW]; COL]);
+        for i in 0..ROW {
+            for j in 0..COL {
+                res[[j, i]] = self[[i, j]];
+            }
+        }
+        res
+    }
+}
+
+#[test]
+fn test_transpose() {
+    let a = Matrix([[0i32, 1i32, 2i32], [3i32, 4i32, 5i32], [6i32, 7i32, 8i32]]);
+    let expect = Matrix([[0i32, 3i32, 6i32], [1i32, 4i32, 7i32], [2i32, 5i32, 8i32]]);
+    let ret = a.t();
+    assert_eq!(ret, expect);
+}
